a3bec9b20ce199226de67bdc6a7883298822b00b
256hz <46080343+256hz@users.noreply.github.com>
2019-03-05 16:49:55 -0800

    Update tweet_shortener.rb
    deleted comments

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -48,17 +48,3 @@ def shortened_tweet_truncator(tweet)
     tweet
   end
 end
-
-    
-#dictionary
-tweet_array = ["Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!",
-
-"OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?",
-
-"I'm running out of example tweets for you guys, which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real.",
-
-"GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!",
-
-"New game. Middle aged tweet followed by #youngPeopleHashTag Example: Gotta get my colonoscopy and mammogram soon. Prevention is key! #swag"]
-
-bulk_tweet_shortener(tweet_array)
